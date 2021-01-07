29cb6a7b460352e42ad4891af96014bd47114989
Cerem Cem ASLAN <cem@aktos.io>
2021-01-08 02:28:12 +0300

    added --no-unnecessary-upstream switch

--- tests/needs-push.sh
+++ tests/needs-push.sh
@@ -16,3 +16,29 @@ check "`$bin --depth=0 x`" "`cat << EOL
 x: Needs push (master) 
 EOL
 `" || break
+
+(
+    cd x
+    git checkout -b some-branch
+)
+
+check "`$bin --depth=0 x`" "`cat << EOL
+x: Needs push (master) Needs upstream (some-branch) 
+EOL
+`" || break
+
+check "`$bin --depth=0 --no-unnecessary-upstream x`" "`cat << EOL
+x: Needs push (master) Needs upstream (some-branch) 
+EOL
+`" || break
+
+(
+    cd x
+    git checkout master
+)
+
+# some-branch has no unique commits, so it doesn't really need an upstream
+check "`$bin --depth=0 --no-unnecessary-upstream x`" "`cat << EOL
+x: Needs push (master) 
+EOL
+`" || break
