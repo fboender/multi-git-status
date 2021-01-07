f20e4db1c1395e7e3d4e15346a4924e58c7ee432
Cerem Cem ASLAN <cem@aktos.io>
2021-01-07 14:49:48 +0300

    fixed tests

--- run-tests.sh
+++ run-tests.sh
@@ -21,14 +21,14 @@ res=$(./mgitstatus --depth=0 "$TESTS_DIR")
 
 # Test 1
 expected=$(cat << EOF
-./tests/a/b/baz: Uncommitted changes 
-./tests/a/bar: Uncommitted changes 
-./tests/foo: Uncommitted changes 
+./tests/a/b/baz: Repo has no commits yet
+./tests/a/bar: Repo has no commits yet
+./tests/foo: Repo has no commits yet
 EOF
 )
 
 [[ "$res" = "$expected" ]] \
-    && echo "Test 1: passed" \
+    && echo "Test 1: 'Repo has no commits yet' passed" \
     || { echo "Test 1: failed."; dump "$expected" "$res"; }
 
 echo "All tests are passed."
