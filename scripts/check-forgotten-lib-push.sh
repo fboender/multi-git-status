1ed8ac55557b1b8c9d9be6d1f68979346c22444f
Cerem Cem ASLAN <cem@aktos.io>
2021-01-08 21:46:29 +0300

    various improvements

--- scripts/check-forgotten-lib-push.sh
+++ scripts/check-forgotten-lib-push.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
-OPTS=
-dir="${1:-~/curr-projects}"
+set -u 
+dir="$1"
 cd "$dir"
 >&2 echo "Checking $dir"
 TIMEFORMAT=%0lR
 time mgitstatus --depth=6 -e --no-unnecessary-upstream --no-pull --only-submodules \
-    --info -c $OPTS . 2>&1 \
+    --info -c . 2>&1 \
     | stdbuf -o 0 grep -v "Permission denied" \
     | stdbuf -o 0 grep -v "File system loop detected"
-#    | grep -v "backups" \
-#    | grep -v "sync-root"
